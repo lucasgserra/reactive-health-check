cf77527af559d5c68030e8ca3c23fb4c1da82d95
Lucas Serra <lucasserradevelopment@gmail.com>
2025-11-01 23:21:49 -0300

    initial commit

---
+++ src/main/java/com/lucasserra/reactive_health_check/ReactiveHealthCheckApplication.java
@@ -0,0 +1,13 @@
+package com.lucasserra.reactive_health_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveHealthCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveHealthCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasserra/reactive_health_check/ReactiveHealthCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasserra.reactive_health_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveHealthCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
